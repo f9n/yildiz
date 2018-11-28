98edb561e94188f7e6f1fef8825ee50a1e373704
f9n <f9n@protonmail.com>
2018-11-28 03:09:54 +0300

    Refactor HomeCard widget

--- lib/screens/home/card.dart
+++ lib/screens/home/card.dart
@@ -9,18 +9,31 @@ class HomeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(5.0),
-      alignment: Alignment.center,
-      child: Column(
-        children: <Widget>[
-          IconButton(
-            icon: Icon(icon),
-            tooltip: text,
-            onPressed: () => Navigator.pushNamed(context, navigation),
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, navigation),
+      child: Card(
+        margin: EdgeInsets.only(bottom: 50.0, left: 10.0, right: 10.0),
+        child: Container(
+          margin: EdgeInsets.all(10.0),
+          padding: EdgeInsets.all(10.0),
+          alignment: Alignment.center,
+          child: Column(
+            children: <Widget>[
+              Icon(icon),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Color(0xCDFFFFFF),
+                child: Text(
+                  text,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
           ),
-          Text(text)
-        ],
+        ),
       ),
     );
   }
